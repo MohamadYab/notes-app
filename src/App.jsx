c6d102ea54db512c4395d6206968b237b1c14ba1
Mohamad Yabroudi <mohamad.syr7@gmail.com>
2025-07-24 01:10:01 +0100

    - Updated App Title - Added a timer to the app header using a combination of useRef + useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import NoteForm from './components/NoteForm';
 import NotesList from './components/NotesList';
 
 function App() {
+  const timerRef = useRef(null);
+
+  const [time, setTime] = useState(new Date());
   const [notes, setNotes] = useState(() => {
     const notes = JSON.parse(localStorage.getItem('notes'));
     return notes || [];
   });
 
+  useEffect(() => {
+    timerRef.current = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timerRef.current);
+      timerRef.current = null;
+    }
+  }, []);
   useEffect(() => {
     localStorage.setItem('notes', JSON.stringify(notes));
   }, [notes]);
@@ -17,11 +30,25 @@ function App() {
     const updatedNotes = notes.filter((note) => note.id !== id);
     setNotes(updatedNotes);
   }
+
+  function getFormattedTimer(time) {
+    const fixZero = (num) => num > 10 ? num : `0${num}`;
+    const hours = fixZero(time.getHours());
+    const minutes = fixZero(time.getMinutes());
+    const seconds = fixZero(time.getSeconds());
+    return `${hours}:${minutes}:${seconds}`;
+  }
+
   return (
     <div
       className='max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg'
     >
-      <h2 className="text-2xl font-bold mb-4 text-center">Notes App</h2>
+      <div className="mb-4 flex justify-between items-center">
+        <h2 className="text-2xl font-bold">Notes App</h2>
+        <p className='text-sm text-gray-600'>
+          <span className='text-xl'>🕰</span> {getFormattedTimer(time)}
+        </p>
+      </div>
       <NoteForm
         notes={notes}
         setNotes={setNotes}
